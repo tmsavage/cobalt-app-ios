77dee40cb2b5f9ba5145b79a5a91db7f83c5d929
tmsavage <tobymsavage@gmail.com>
2024-11-22 21:36:06 -0500

    initial commit

---
+++ Cobalt/Cobalt/CobaltApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaltApp.swift
+//  Cobalt
+//
+//  Created by Toby Savage on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CobaltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
